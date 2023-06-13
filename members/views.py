123391e8221eb9b0fab2f34cab9ec84ce782b550
FabioPortella <fabioportella2009@gmail.com>
2023-06-13 19:50:24 -0300

    primeiro comit

---
+++ members/views.py
@@ -0,0 +1,64 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from .models import Member
+from .forms import MembersForm
+
+
+def members(request):
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('all_members.html')
+    context = {
+        'mymembers': mymembers,
+    }
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember,
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def add(request):
+    template = loader.get_template('add.html')
+    return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+    first = request.POST['first']
+    last = request.POST['last']
+    phone = request.POST['phone']
+
+    member = Member(firstname=first, lastname=last, phone=phone)
+    member.save()
+    return HttpResponseRedirect(reverse('members'))
+
+def update(request, id):
+    member = Member.objects.get(id=id)
+    template = loader.get_template('update.html')
+    context = {
+        'member': member,
+    }
+    return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+    first = request.POST['first']
+    last = request.POST['last']
+    phone = request.POST['phone']
+    member = Member.objects.get(id=id)
+    member.firstname = first
+    member.lastname = last
+    member.phone = phone
+    member.save()
+    return HttpResponseRedirect(reverse('members'))
+
+def delete(request, id):
+    member = Member.objects.get(id=id)
+    member.delete()
+    return HttpResponseRedirect(reverse('members'))
